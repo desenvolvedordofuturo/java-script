c69d2bcb7519ba250b6bedd02665b94e5699c604
Washington programador <122320466+desenvolvedordofuturo@users.noreply.github.com>
2023-02-09 16:28:24 -0300

    nodejs

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) { // Esta Condição é Simples. Por que se for verdadeira acontece!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
