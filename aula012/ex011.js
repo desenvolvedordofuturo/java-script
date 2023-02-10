42e83e78491dfdf59c170f051a791a79afc371e9
Washington programador <122320466+desenvolvedordofuturo@users.noreply.github.com>
2023-02-10 11:14:43 -0300

    exercicio 11

---
+++ aula012/ex011.js
@@ -0,0 +1,8 @@
+var idade = 64
+if (idade < 16) {
+    console.log(`Não Vota`)
+} else if (idade < 18 || idade > 65) {
+    console.log(`Voto opcional`)
+} else {
+       console.log('Voto obrigatório')
+}
