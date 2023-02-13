9b24aa071ebc4afe47607a637f8726636d2271cc
Washington programador <122320466+desenvolvedordofuturo@users.noreply.github.com>
2023-02-13 15:57:49 -0300

    atualizado

--- modelo/ex014/ex014/script.js
+++ modelo/ex014/ex014/script.js
@@ -2,21 +2,22 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+   // var hora = data.getHours()
+   var hora = 23
     msg.innerHTML = `Agora são ${hora}horas.`
 
-    if (hora >= 0 && hora < 12) {
+    if (hora >=  0 && hora <  12) {
         //Bom Dia!
-        img.src = 'manha.png'
+        img.src = 'fotomanha.png'
         document.body.style.background = '#e2cd9f'
 
-    } else if (hora >= 12 && hora < 18) { 
+    } else if (hora >=  12 && hora <  18) { 
         //Boa Tarde!
-        img.src = 'tarde.png'
+        img.src = 'fototarde.png'
         document.body.style.background = '#b9846f' 
     } else {
          //Boa Noite!
-        img.scr = 'noite.png'
+        img.scr = 'fotonoite.png'
         document.body.style.background = '#515154'
     }
 }
