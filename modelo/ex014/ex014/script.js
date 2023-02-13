deef7fd3568184641000d0600521685d7d3a94a2
Washington programador <122320466+desenvolvedordofuturo@users.noreply.github.com>
2023-02-13 16:36:10 -0300

    atualizado

--- modelo/ex014/ex014/script.js
+++ modelo/ex014/ex014/script.js
@@ -3,7 +3,7 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
    // var hora = data.getHours()
-   var hora = 15
+   var hora = 22
     msg.innerHTML = `Agora são ${hora}horas.`
 
     if (hora >=  0 && hora <  12) {
