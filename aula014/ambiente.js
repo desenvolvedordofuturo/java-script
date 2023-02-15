9541b2ec4dc3c7efb2b85b0648d44695ee249f18
Washington programador <122320466+desenvolvedordofuturo@users.noreply.github.com>
2023-02-15 01:14:10 -0300

    aula014

---
+++ aula014/ambiente.js
@@ -0,0 +1,23 @@
+var C = 1
+do {
+    console.log(`Passo ${C}`)
+    C++
+} while (C <= 6)
+
+/*
+var C = 1
+while (C <= 100) {
+    console.log(`Passo ${C}`)
+    C++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
